ebe60c39db4cfb5bf68931c071ae21ef8082aa59
PadBol1742 <padbol1030@gmail.com>
2026-03-23 13:18:47 +0100

    Register page fonctionne globalement comme voulu. Reste juste le Css le cas échéant

--- src/pages/auth/Register.page.tsx
+++ src/pages/auth/Register.page.tsx
@@ -1,17 +1,19 @@
 import { useActionState } from "react";
 import z from 'zod';
-import type { MemberDto } from '../../@types/member';
-import {PushRegistration} from '../../services/auth/auth.service';
+import { PushRegistration } from '../../services/auth/auth.service';
 import { toast } from "sonner";
+import { Link } from "react-router";
+import type { MemberState, NewMemberDto } from "../../@types/member";
+
 
 
 // Validation Scheme (zod)
-const MemberScheme =
+const NewMemberScheme =
     z.object({
         nick: z.string()
             .trim()
             .max(50, { error: 'Un pseudo doit faire entre 3 et 50 caractères !' })
-            .nullable(),   //? Ou optionnal ?
+            .nullable(),   // TODO Check : Ou optionnal ?
         email: z.email({ error: "L'adresse email n'a pas un format valide !" })
             .toLowerCase(),
         pwd1: z.string()
@@ -23,47 +25,33 @@ const MemberScheme =
 
 
 // Extraction of type
-export type MemberData = z.infer<typeof MemberScheme>;
+// export type NewMemberData = z.infer<typeof NewMemberScheme>;
+//? Inutile jusqu'ici
 
-
-// Componant
-type MemberState = {
-    formData: FormData | null,
-    error: {
-        nick?: string[];
-        email?: string[];
-        pwd1?: string[];
-        pwd2?: string[];
-    } | null;
-};
-
-
-  // Accessibility Id
-//   const inputId = useId(); //TODO : Pourquoi Uncaught TypeError: can't access property "useId", resolveDispatcher() is null ?
 
 export default function RegisterPage() {
 
     // form Action
-    const onRegisterSubmit = async (state: MemberState, formData: FormData) => {
+    const onRegisterSubmit = async (_state: MemberState, formData: FormData): Promise<MemberState> => {
 
         //data validation w. zod
-        const { data, error, success } = MemberScheme.safeParse(Object.fromEntries(formData.entries())); // TODO : Check pq en async ça déconne ?
+        const { data, error, success } = await NewMemberScheme.safeParseAsync(Object.fromEntries(formData.entries()));
 
 
         // Error handling
         if (!success) {
             return {
                 formData,
-                error: z.flattenError(error).fieldErrors // TODO : Check pq pour une erreur ttes les erreurs sont levée ?
+                error: z.flattenError(error).fieldErrors
             };
         }
 
         // Mapping
-        const newMember: MemberDto = {
+        const newMember: NewMemberDto = {
             nick: data.nick,
             email: data.email,
             password: data.pwd1,
-        }
+        };
 
         // Service calling
         const result = await PushRegistration(newMember);
@@ -71,19 +59,24 @@ export default function RegisterPage() {
         // Check dev
         console.log(newMember, result);
 
-        if(result.success) {
-            toast.success(result.data.message)
+        if (result.success) {
+            toast.success(result.data.message);
 
             return {
                 formData: null,
                 error: null
             };
+        } else {
+            toast.error(result.error);
+
+            return {
+                formData,
+                error: {
+                    server: result.error
+                }
+            };
         }
-        else {
-            toast.error(result.error);            
-            return state; // TODO : Check pq le formData ne repasse pas, càd garde pas les nick et email pour le default ?
-        }
-        
+
     };
 
     // Utilisation
@@ -91,29 +84,37 @@ export default function RegisterPage() {
     const [state, handleSubmit, isPending] = useActionState(onRegisterSubmit, { formData: null, error: null });
 
     return (
-        <form className='form' action={handleSubmit}>
-            <div>
-                <label htmlFor={'nick'}>Pseudo : </label>
-                <input type='text' id={'nick'} name='nick' defaultValue={state.formData?.get('nick')?.toString()} />
-                {state.error?.nick && (<span>{state.error.nick.join(', ')}</span>)}
-            </div>
-            <div>
-                <label htmlFor={"email"}>Email* : </label>
-                <input type="email" id={'email'} name='email' placeholder='ex: user@example.com' required defaultValue={state.formData?.get('email')?.toString()} />
-                {state.error?.email && (<span>{state.error.email.join(', ')}</span>)}
+        <>
+            <h2>Inscritions</h2>
+            <form className='form' action={handleSubmit}>
+                <div>
+                    <label htmlFor={'nick'}>Pseudo : </label>
+                    <input type='text' id={'nick'} name='nick' defaultValue={state.formData?.get('nick')?.toString()} />
+                    {state.error?.nick && (<span>{state.error.nick.join(', ')}</span>)}
+                </div>
+                <div>
+                    <label htmlFor={"email"}>Email* : </label>
+                    <input type="email" id={'email'} name='email' placeholder='ex: user@example.com' required defaultValue={state.formData?.get('email')?.toString()} />
+                    {state.error?.email && (<span>{state.error.email.join(', ')}</span>)}
 
-            </div>
-            <div>
-                <label htmlFor={'pwd1'}>Choix du Mot de Passe* :</label>
-                <input type='text' id={'pwd1'} name='pwd1' placeholder='ex: Test123!' required />
-                {state.error?.pwd1 && (<span>{state.error.pwd1.join(', ')}</span>)}
-            </div>
-            <div>
-                <label htmlFor={'pwd2'}>Confirmation du Mot de Passe* :</label>
-                <input type='text' id={'pwd2'} name='pwd2' placeholder='ex: Test123!' required />
-                {state.error?.pwd2 && (<span>{state.error.pwd2.join(', ')}</span>)}
-            </div>
-            <button disabled={isPending} className='btn-form' type='submit'>S'enregistrer</button>
-        </form>
+                </div>
+                <div>
+                    <label htmlFor={'pwd1'}>Choix du Mot de Passe* :</label>
+                    <input type='password' id={'pwd1'} name='pwd1' placeholder='ex: Test123!' required />
+                    {state.error?.pwd1 && (<span>{state.error.pwd1.join(', ')}</span>)}
+                </div>
+                <div>
+                    <label htmlFor={'pwd2'}>Confirmation du Mot de Passe* :</label>
+                    <input type='password' id={'pwd2'} name='pwd2' placeholder='ex: Test123!' required />
+                    {state.error?.pwd2 && (<span>{state.error.pwd2.join(', ')}</span>)}
+                </div>
+                <button disabled={isPending} className='btn-form' type='submit'>S'enregistrer</button>
+                {state.error?.server && (<span>{state.error.server}</span>)}
+            </form>
+            <p>
+                Déjà Inscrit ?
+                <Link to='../'><button className='btn-link'>Connectez-vous ici !</button></Link>
+            </p>
+        </>
     );
 } 
